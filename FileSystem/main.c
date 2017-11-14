3a4ca728bd84049398c0ec650aaa14d55e04fa2e
pedroeml <pedro.elpidio@acad.pucrs.br>
2017-11-14 09:15:45 -0200

    Included headers at main

--- FileSystem/main.c
+++ FileSystem/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
+#include "dir/dir_entry.h"
+#include "cluster/data_cluster.h"
 
 uint16_t fat[4096]; // FAT's table 8 clusters, 4096 inputs of 16 bits (8192 bytes)
 
